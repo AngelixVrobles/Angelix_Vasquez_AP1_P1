acda9b4c39283a29c4963edba14bb27c27f2ee93
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-06-02 19:47:21 -0400

    agregar contexto

---
+++ Angelix_Vasquez_AP1_P1/DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Angelix_Vasquez_AP1_P1.DAL
+{
+    public class Contexto : DbContext
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options))
+    {
+    }
+}
